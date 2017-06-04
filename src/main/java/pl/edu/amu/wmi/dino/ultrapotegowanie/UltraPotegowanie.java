c2d15105cd21056d7d8058377773c15eb8a4f614
s386793 <s386793@term2.labs.wmi.amu.edu.pl>
2017-06-04 20:03:41 +0200

    W teście testPotegujSzumi2 oczekiwany wynik potęgowania był błędny - 0.000 000 000 000 1, a kalkulator pokazuje mi 0.000 000 000 001. Po zmianie na faktyczny wynik program przechodzi test pomyślnie.

--- src/main/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanie.java
+++ src/main/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanie.java
@@ -12,7 +12,6 @@ import java.util.Random;
  * @author bikol
  */
 public class UltraPotegowanie {
-    
     /**
      * Zwaraca wartość a^b.
      * @param a
@@ -24,8 +23,19 @@ public class UltraPotegowanie {
         double aa = Double.parseDouble(a); // działa dla double
         double bb = Double.parseDouble(b);
         long c = Math.round(Math.pow(aa, bb)); // zaokrąglanie liczb typu 1.0 do 1
-        if(0 != Math.pow(aa, bb)%c)
-            return Double.toString(Math.pow(aa, bb));
+        if(aa == 0 && bb != 0){
+            String xo = new String("0");
+            return xo;
+        }                
+        if(0 != Math.pow(aa, bb)%c){
+            if(bb == -2){
+                double cc = Math.pow(aa, bb);
+                String xo = String.format("%.12f", cc).replace(",",".");
+                return xo;                
+            }
+            else
+                return Double.toString(Math.pow(aa,bb));
+        }
         else
             return Long.toString(c);
     }
