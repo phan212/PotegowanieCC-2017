ee255e03ef0b58474219d7716646b33e1487d166
aagathaa <agatha1760@gmail.com>
2017-06-05 23:36:04 +0200

    Update UltraPotegowanie.java

--- src/main/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanie.java
+++ src/main/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanie.java
@@ -20,8 +20,36 @@ public class UltraPotegowanie {
      * @return 
      */
     public static String poteguj(String a, String b){
-        double aa = Double.parseDouble(a);
-        double bb = Double.parseDouble(b);
+        char[] checka = a.toLowerCase().toCharArray();
+        char[] checkb = b.toLowerCase().toCharArray();
+        char x = 'x';
+        
+        double aa;        
+        if (a.length() != 1) {
+            if (checka[1] == x) {
+                String tempa = a.substring(2);
+                long na = Long.parseLong(tempa, 16);
+                aa = (double)na;
+            } else {
+                aa = Double.parseDouble(a);
+            }
+        } else {
+            aa = Double.parseDouble(a);
+        }
+        
+        double bb;
+        if (b.length() != 1) {
+            if (checkb[1] == x) {
+                String tempb = b.substring(2);
+                long nb = Long.parseLong(tempb, 16);
+                bb = (double)nb;
+            } else {
+                bb = Double.parseDouble(b);
+            }
+        } else {
+            bb = Double.parseDouble(b);
+        }
+        
         double power = Math.pow(aa, bb);
         if (power % 1 == 0)
                return Long.toString(Math.round(power));
